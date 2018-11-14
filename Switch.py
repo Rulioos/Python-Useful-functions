f7792a5b62ad53a69ae6fb927a9d026a1511cb4e
Aubier Jules <33544567+Rulioos@users.noreply.github.com>
2018-11-15 10:51:20 +1100

    Update Switch
    Deleted switch_dict
    Updated Check_type
    Added switch_int 
    Added make_int_conditons

--- Switch.py
+++ Switch.py
@@ -63,10 +63,13 @@ def switch(typ,default,cases):
     def make_condition(cle):
         return lambda x: x==cle
     
-    def check_type(kind,value):
-        """Check a value type"""
-        if type(value) is kind: return True
-        else: return False
+    ddef check_type(kind, value):
+    """Check a value type"""
+    try:
+        kind(value)
+        return True
+    except ValueError:
+        return False
     
     #checking typ is a callable type
     if not callable(typ):return "{} is not a callable type".format(typ)
@@ -108,40 +111,65 @@ s=switch(str,default="Can't understand your words sorry",cases={"Hi":"Hello Worl
 a=s("Hi")
 
 
-#switch with dicts
-def switch_dict(typ, default, cases):
+
+
+def make_int_condition(key):
+    """
+    Creates equalities and inequalities from requests.
+
+    :param key:Keys possibles shapes are:
+        -"0<<12"
+        -"0<=<=12"
+        -">4" (same for < ) and "<=4"
+        - "=4"
+    :return: a function that holds the wanted condition
+    """
+
+    #equality
+    if key.startswith("="):
+        if check_type(int,key[1:]):
+            return lambda x: x == int(key[1:])
+        else: return TypeError
+
+    #Double inequalities
+    if key.startswith("<") and check_type(int, key[1:]):
+        return lambda x: x < int(key[1:])
+    if key.startswith("<=") and check_type(int, key[2:]):
+        return lambda x: x <= int(key[2:])
+    if key.startswith(">") and check_type(int, key[1:]):
+        return lambda x: x >int(key[1:])
+    if key.startswith(">=") and check_type(int, key[2:]):
+        return lambda x: x >= int(key[2:])
+
+    #triple inequalities
+    possible_exp=[r"(.)+<<(.)+",r"(.)+<=<(.)+",r"(.)+<<=(.)+",r"(.)+<=<=(.)+"]
+    for i,item in enumerate(possible_exp):
+        if re.match(item,key) is not None:
+            Values=re.findall("([0-9]+)",key)
+            if i == 0: return lambda x: x in range(int(Values[0])+1,int(Values[1]))
+            if i == 1: return lambda x: x in range(int(Values[0]), int(Values[1]))
+            if i == 2: return lambda x: x in range(int(Values[0]) +1, int(Values[1])+1)
+            if i == 3: return lambda x: x in range(int(Values[0]), int(Values[1])+1)
+
+    return KeyError
+
+
+
+
+
+def switch_int(default, cases):
     """
     Supported types are:
         -int
-        -str
-     Features only equality case for now.
      
-    :param type: The type of value who will be tested in the switch ( int, str, float ,bin ,list )
     :param default: The default action to take if other conditions are not done
-    :param cases: value=action
+    :param cases: Dict.Keys should have shape "0<<12","<12","=4",">14","0<=<12","0<<=12","0<=<=12".
+                   Items could be anything.
     :return: A switch method to call
     """
-
-    def check_type(kind, value):
-        """Check a value type"""
-        if type(value) is kind:
-            return True
-        else:
-            return False
-
-    # checking typ is a callable type
-    if not callable(typ): return "{} is not a callable type".format(typ)
-    Layers = dict()
-
-    # creating our layers of conditions
-    for key, act in cases.items():
-        my_key = key
-        if not check_type(typ, my_key):
-            print(TypeError, "Key {} should be type {} but is type {} instead".format(key, typ, type(key)))
-            return TypeError
-        Layers[key] = act
-
-    # constructing the switch
+    Layers=[]
+    for key,item in cases.items():
+        Layers.append([make_int_condition(key),item])
     def construct_switch(input_value):
         """
         The switch is constrcuted as a couch of if layers where each leads to an action.
@@ -150,13 +178,28 @@ def switch_dict(typ, default, cases):
         :return: The action to make
         """
         # checking input type
-        if not check_type(typ, input_value):
-            return ValueError, "switch type value is {}".format(typ)
+        if not check_type(int, input_value):
+            return ValueError, "switch type value is {}".format(int)
 
         # checking each layer
-        try:
-            return Layers[input_value]
-        except KeyError:
-            return default
+        for layer in Layers:
+            if layer[0](input_value):
+                return layer[1]
+            else:
+                pass
+                # returns default condition if no others works
+        return default
+
 
     return construct_switch
+
+#exemples
+cases={"0<<12":lambda :print("0_12"),"=13":lambda :print(13),">=14":lambda : print("14 et +")}
+s=switch_int(default=lambda :print('ok'),cases=cases)
+s(5)()
+s(13)()
+s(14)()
+s(15)()
+s(-1)()
+
+
